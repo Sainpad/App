0e35f7c3f7b07a101cfeb2095395cfa05b947e2e
Abdul Rauf <you@example.com>
2025-06-16 09:54:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app= express();
+const port=5000;
+
+
+app.listen(port,()=>
+{
+    console.log("i am new we developer,now iam learning that how can we great new repositry")
+})
