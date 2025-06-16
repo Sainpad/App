7ca5aa6351a2094a03af6a6d2beba876ae6a527d
Abdul Rauf <you@example.com>
2025-06-16 10:01:37 +0500

    seond commit

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const port=5000;
 
 app.listen(port,()=>
 {
-    console.log("i am new we developer,now iam learning that how can we great new repositry")
+    console.log("i am new we developer,now iam learning that how can we great new repositry,i am very pro developer')
 })
